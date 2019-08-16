5f33d78c900c04749a71ad96be9d3e297967ad6a
mkdoh3 <mkdoherty3@gmail.com>
2019-08-16 13:32:08 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,8 @@
 # require '../../config/environment'
 
 class App < Sinatra::Base
+  set :views, Proc.new { File.join(root, '../views/') }
 
-  set :views, Proc.new { File.join(root, "../views/") }
-  
   get '/' do
     erb :super_hero
   end
